fb1fb25dedbb27e8a3a4f4b021362b4919985cbd
Sotatek-SonDo2 <son.do2@sotatek.com>
2025-05-22 12:07:34 +0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,71 @@
+/**
+ * Array methods:
+ * forEach()
+ * every()
+ * some()
+ * find()
+ * filter()
+ */
+/*
+var course = [
+    {
+        id: 1,
+        name: 'Javascript',
+        coin: 0
+
+    },
+      {
+        id: 2,
+        name: 'hHTML, CSS',
+        coin: 1
+    },
+    {
+        id: 3,
+        name: 'Ruby',
+        coin: 1
+    },
+    {
+        id: 4,
+        name: 'PHP',
+        coin: 200
+    },
+    {
+        id: 5,
+        name: 'ReactJS',
+        coin: 1
+    },
+    {
+        id: 6,
+        name: 'Ruby',
+        coin: 10
+    },
+    {
+        id: 7,
+        name: 'ReactJS',
+        coin: 12
+    },
+];
+
+//co the dung duyet qua tung mang
+course.forEach(function( course, index) {
+    console.log(indexedDB, course);
+});
+
+//thoa man 1 dk dung
+var isFree = course.every(function( course, index) {
+    return course.coin === 0;
+});
+
+console.log(isFree);
+
+//1 mang tro ve dung
+var isFree = courses.some(function( course, index) {
+    return course.coin === 0;
+});
+
+//danh sach
+var listcourses = courses.find(function( course, index) {
+    return course.coin === 'Ruby';
+});
+
+console.log(console);*/
---
+++ main3.js
@@ -0,0 +1,21 @@
+//includes method
+
+// var courses = ['Javascript', 'PHP', 'Dart' ];
+
+// //3 + -1
+// console.log(courses.includes('Javascript', -3));
+
+/*
+
+Math object
+
+Math.PI //so thp phan
+Math.round() //lm tron
+Math.abs() // tra gia tri tuyet doi
+Math.ceil() //lam tron tren
+Math.floor() //lam tron duoi
+Math.random() 
+Math.min() 
+Math.max() 
+*/
+console.log(Math.max - Math.min);
